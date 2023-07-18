fb2a362d720a106609e5c942949f334b04121ab1
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2023-07-18 01:24:24 +0100

    refactor selenium code to be function driven

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # BorrowBox-Scraper
 Python script to hands-free download all audiobooks on loan from library BorrowBox account.
+
+This script automates the process of downloading books from BorrowBox.
+It uses Selenium WebDriver to log into BorrowBox, navigate to the "My Loans" page, and download all books.
+
+## Requirements
+
+- Python 3
+- Selenium
+- ChromeDriver
+- WebDriver Manager for Python
+
+## Usage
+
+1. Install the required Python packages:
+
+```
+pip install selenium webdriver_manager
+```
+
+2. Replace `<Library Card Number>` and `<Pass Code>` in the `login` function with your actual BorrowBox library card number and pass code.
+
+3. Run the script:
+
+    ```
+    python scrape_borrowbox_books.py
+    ```
+
+
+The script will log into BorrowBox, navigate to the "My Loans" page, and download all books.
+The books will be downloaded to the default download directory of your Chrome browser.
+
+## Testing
+
+The script includes a unit test for the `login` function. To run the test, use the following command:
+
+    ```
+    python -m unittest test_scrape_borrowbox_books.py
+    ```
--- scrape_borrowbox_books.py
+++ scrape_borrowbox_books.py
@@ -6,79 +6,77 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 import pickle
 
-# Setup Chrome options
-chrome_options = Options()
-# chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_experimental_option("detach", True)
+# User Info
+LIBRARY_LOCATION = 'hampshire'
+LIBRARY_CARD_NUMBER = '<Library Card Number>'
+PASSCODE = '<Pass Code>'
 
-# Set path to chromedriver as per your configuration
-webdriver_service = Service(ChromeDriverManager().install())
-
-# Choose Chrome Browser
-driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-
-driver.get('https://hampshire.borrowbox.com/login')
-
-# Assuming username field has id 'username' and password field has id 'password'
-username = driver.find_element(By.ID, 'form-input-1')
-password = driver.find_element(By.ID, 'form-input-2')
-
-# Replace 'your_username' and 'your_password' with your actual username and password
-username.send_keys('<Library Card Number>')
-password.send_keys('<Pass Code>')
-
-# Assuming login button has id 'login'
-login_button = driver.find_element(By.XPATH, '//button[@type="submit" and contains(@class, "signin-button")]')
-login_button.click()
-
-# Wait for page to load
-time.sleep(5)
-
-# Save cookies
-pickle.dump(driver.get_cookies() , open("cookies.pkl","wb"))
-
-# Load cookies
-cookies = pickle.load(open("cookies.pkl", "rb"))
-for cookie in cookies:
-    driver.add_cookie(cookie)
-
-# Navigate to My Loans page
-driver.get('https://hampshire.borrowbox.com/home/my-loans') # Replace with actual My Loans URL
-time.sleep(5)
+# Constants
+LOGIN_URL = f"https://{LIBRARY_LOCATION}.borrowbox.com/login"
+MY_LOANS_URL = f"https://{LIBRARY_LOCATION}.borrowbox.com/home/my-loans"
+DOWNLOAD_BUTTON_CLASS = 'button-download'
+PROGRESS_BUTTON_CLASS = 'download-button'
+USERNAME_FIELD_ID = 'form-input-1'
+PASSWORD_FIELD_ID = 'form-input-2'
+LOGIN_BUTTON_XPATH = '//button[@type="submit" and contains(@class, "signin-button")]'
+COOKIE_FILE = "cookies.pkl"
+WAIT_TIME = 5
+DOWNLOAD_WAIT_TIME = 60
 
 
-# Get all download buttons
-download_buttons = driver.find_elements(By.CLASS_NAME, 'button-download')
-nb_downloads = len(download_buttons)
-print(download_buttons)
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_experimental_option("detach", True)
+    webdriver_service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Loop over download buttons
-for i in range(nb_downloads):
-    driver.get('https://hampshire.borrowbox.com/home/my-loans')
-    time.sleep(5)
+def login(driver, username, password):
+    driver.get(LOGIN_URL)
+    username_field = driver.find_element(By.ID, USERNAME_FIELD_ID)
+    password_field = driver.find_element(By.ID, PASSWORD_FIELD_ID)
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    login_button = driver.find_element(By.XPATH, LOGIN_BUTTON_XPATH)
+    login_button.click()
+    time.sleep(WAIT_TIME)
 
-    # Click download button
-    download_buttons = driver.find_elements(By.CLASS_NAME, 'button-download')
-    button = download_buttons[i]
-    button.click()
+def save_cookies(driver):
+    pickle.dump(driver.get_cookies(), open(COOKIE_FILE, "wb"))
 
-    # Wait for page to load
-    time.sleep(5)
+def load_cookies(driver):
+    cookies = pickle.load(open(COOKIE_FILE, "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
 
-    # Find and click the "progress-button"
-    progress_button = driver.find_element(By.CLASS_NAME, 'download-button')
-    progress_button.click()
-    time.sleep(60)
+def navigate_to_my_loans(driver):
+    driver.get(MY_LOANS_URL)
+    time.sleep(WAIT_TIME)
 
-    driver.get('https://hampshire.borrowbox.com/home/my-loans')
-    time.sleep(5)
+def download_books(driver):
+    download_buttons = driver.find_elements(By.CLASS_NAME, DOWNLOAD_BUTTON_CLASS)
+    for i in range(len(download_buttons)):
+        driver.get(MY_LOANS_URL)
+        time.sleep(WAIT_TIME)
+        download_buttons = driver.find_elements(By.CLASS_NAME, DOWNLOAD_BUTTON_CLASS)
+        download_buttons[i].click()
+        time.sleep(WAIT_TIME)
+        progress_button = driver.find_element(By.CLASS_NAME, PROGRESS_BUTTON_CLASS)
+        progress_button.click()
+        time.sleep(DOWNLOAD_WAIT_TIME)
 
-# quit the chrome driver now books are downloaded
-driver.quit()
+def main():
+    driver = setup_driver()
+    login(driver, LIBRARY_CARD_NUMBER, PASSCODE)
+    save_cookies(driver)
+    load_cookies(driver)
+    navigate_to_my_loans(driver)
+    download_books(driver)
+    driver.quit()
 
+if __name__ == "__main__":
+    main()
